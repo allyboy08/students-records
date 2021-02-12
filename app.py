4dbee5a5bad2e0433867296579072e0fea8210cd
allyboy08 <alexsassman65@gmail.com>
2021-02-12 09:26:44 +0200

    Updated.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def init_sqlite_db():
 
 init_sqlite_db()
 app = Flask(__name__)
-
+@app.route('/')
 @app.route('/enter-new/')
 def enter_new_student():
     return render_template('students.html')
@@ -36,6 +36,22 @@ def add_new_record():
         finally:
             con.close()
             return render_template('result.html', msg=msg)
+
+@app.route('/show-records/', methods=["GET"])
+def show_records():
+    records = []
+    try:
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM students")
+            records = cur.fetchall()
+    except Exception as e:
+        con.rollback()
+        print("There was am error fetching results from the database.")
+    finally:
+        con.close()
+        return render_template('records.html', records=records)
+
 
 if __name__=='__main__':
     app.run(debug=True)
